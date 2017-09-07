d9cdff812ba1098d0935ff721987bc4696ece230
Nicolas <triperodericota@users.noreply.github.com>
2017-09-07 19:32:32 -0300

    Update ejer12.rb

--- Practica1/ejer12.rb
+++ Practica1/ejer12.rb
@@ -1,8 +1,8 @@
 def rot(anString, aNumber)
 	letters = ('a'..'z').to_a
 	encryted_string = ''
-	valid_block = case aNumber when aNumber > 25 then {|a_char| encryted_string + letters[ 25 - (letters.index(a_char))] }
-	else {|a_char| encryted_string + letters[letters.index(a_char)] }
+	valid_block = case aNumber when aNumber > 25 then {|a_char| encryted_string << letters[ 25 - (letters.index(a_char))] }
+	else {|a_char| encryted_string << letters[letters.index(a_char) + aNumber] }
 	anString.each_char {|a_char| encryted_string + letters[ 25 - (letters.index(a_char))] }
 
 end
